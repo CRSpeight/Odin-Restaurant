6086c4e74fa39f6366b84c67134b46f1ee16683d
Chris Speight <paradisio15@gmail.com>
2020-08-29 21:12:45 -0500

    basic layout/placeholders created and JS injection added

---
+++ src/homepage.js
@@ -0,0 +1,8 @@
+const homepageContent = {
+  title: "Aegir's Tavern",
+  headline: "Best Mead this side of Midgard!",
+  content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim praesent elementum facilisis leo vel fringilla. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Et molestie ac feugiat sed lectus vestibulum mattis. Ipsum faucibus vitae aliquet nec. Ullamcorper a lacus vestibulum sed arcu non. Imperdiet dui accumsan sit amet nulla facilisi. Vitae justo eget magna fermentum iaculis eu non. Mattis vulputate enim nulla aliquet porttitor lacus. Leo duis ut diam quam nulla porttitor massa. Adipiscing vitae proin sagittis nisl. Ipsum faucibus vitae aliquet nec ullamcorper sit.",
+};
+
+export { homepageContent };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("TEST!");
+import { homepageContent } from "./homepage";
+
+const content = document.getElementById("content");
+
+let h1 = document.createElement("h1");
+h1.textContent = homepageContent.title;
+content.appendChild(h1);
+
+let headline = document.createElement("h3");
+headline.textContent = homepageContent.headline;
+content.appendChild(headline);
+
+let pagecontent = document.createElement("p");
+pagecontent.textContent = homepageContent.content;
+content.appendChild(pagecontent);
